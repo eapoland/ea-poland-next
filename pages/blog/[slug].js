718414ec6528233d6d97572f7da4a196fe22af9b
Michał Hawełka <m.hawelka@gmail.com>
2022-09-26 07:52:07 +0200

    SEO fro blog

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -2,6 +2,7 @@ import { gql } from "@apollo/client";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Head from "next/head";
 import client from "../../apollo-client";
+import parse from "html-react-parser";
 
 export async function getStaticPaths({ locales }) {
   const { data } = await client.query({
@@ -44,33 +45,7 @@ export default function Post({ postData }) {
 
   return (
     <div className="mt-20 flex flex-col items-center gap-16">
-      {/* <Head>
-        <title>{postData.attributes.title} - Hawelka.dev</title>
-        {postData.attributes.SEO && (
-          <>
-            <meta
-              name="description"
-              content={postData.attributes.SEO.metaDescription}
-            />
-            <meta property="og:title" content={postData.attributes.title} />
-            <meta property="og:type" content="article" />
-            <meta
-              property="og:image"
-              content={postData.attributes.SEO.metaImage.data.attributes.url}
-            />
-            <meta
-              property="twitter:image"
-              content={
-                postData.attributes.SEO.metaTwitterImage.data.attributes.url
-              }
-            />
-            <meta property="twitter:card" content="summary_large_image" />
-          </>
-        )}
-      </Hea>
-      <ReactMarkdown className="prose prose-sm md:prose-lg lg:prose-xl xl:prose-2xl prose-slate w-11/12 mx-4 my-8">
-        {postData.attributes.content}
-      </ReactMarkdown> */}
+      <Head>{postData ? parse(postData.seo.fullHead) : ""}</Head>
       <div
         className="flex flex-col justify-center items-center w-full gap-6 h-[20rem] md:h-[30rem]"
         style={{
